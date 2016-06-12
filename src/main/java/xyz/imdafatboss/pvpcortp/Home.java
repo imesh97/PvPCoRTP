a8f4c081150fd95316aad4244b744918d1206ed1
imesh97 <imesh.nimsitha@gmail.com>
2016-06-12 11:15:38 -0400

    Initialized the FileManager class.

--- src/main/java/xyz/imdafatboss/pvpcortp/Home.java
+++ src/main/java/xyz/imdafatboss/pvpcortp/Home.java
@@ -2,13 +2,19 @@ package xyz.imdafatboss.pvpcortp;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.pvpcortp.config.FileManager;
 
 public class Home extends JavaPlugin implements Listener{
+
+    FileManager fm;
 
     @Override
     public void onEnable(){
 
+        fm = new FileManager(this);
         getLogger().info("Created by imdafatboss");
+
+        fm.getConfig("config.yml").saveDefaultConfig();
 
     }
 
