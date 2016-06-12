417edd64986535900da5a55333d8fac8d03c1b99
imesh97 <imesh.nimsitha@gmail.com>
2016-06-12 12:33:54 -0400

    Added the command 'rtp'.

--- src/main/java/xyz/imdafatboss/pvpcortp/Home.java
+++ src/main/java/xyz/imdafatboss/pvpcortp/Home.java
@@ -2,19 +2,25 @@ package xyz.imdafatboss.pvpcortp;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.pvpcortp.cmd.RTPCmd;
 import xyz.imdafatboss.pvpcortp.config.FileManager;
 
 public class Home extends JavaPlugin implements Listener{
 
     FileManager fm;
+    RTPCmd rtp;
 
     @Override
     public void onEnable(){
 
         fm = new FileManager(this);
+        rtp = new RTPCmd(this);
         getLogger().info("Created by imdafatboss");
 
         fm.getConfig("config.yml").saveDefaultConfig();
+        
+        rtp.getCommands();
+        rtp.getAliases();
 
     }
 
