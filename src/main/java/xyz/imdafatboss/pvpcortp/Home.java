a0d7d067103174630bd056759893f0cdfb4068a6
imesh97 <imesh.nimsitha@gmail.com>
2016-06-12 15:12:54 -0400

    Registered the listeners.

--- src/main/java/xyz/imdafatboss/pvpcortp/Home.java
+++ src/main/java/xyz/imdafatboss/pvpcortp/Home.java
@@ -1,26 +1,34 @@
 package xyz.imdafatboss.pvpcortp;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.imdafatboss.pvpcortp.cmd.RTPCmd;
 import xyz.imdafatboss.pvpcortp.config.FileManager;
+import xyz.imdafatboss.pvpcortp.events.RTPEvent;
 
 public class Home extends JavaPlugin implements Listener{
 
     FileManager fm;
     RTPCmd rtp;
+    RTPEvent evt;
 
     @Override
     public void onEnable(){
 
         fm = new FileManager(this);
-        rtp = new RTPCmd(this);
-        getLogger().info("Created by imdafatboss");
-
         fm.getConfig("config.yml").saveDefaultConfig();
-        
+
+        rtp = new RTPCmd(this);
+        evt = new RTPEvent(this);
+        getLogger().info("Created by imdafatboss");
+        PluginManager pm = Bukkit.getPluginManager();
+
         rtp.getCommands();
         rtp.getAliases();
+
+        pm.registerEvents(evt, this);
 
     }
 
